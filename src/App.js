08c8d7b65cff12c252bffc9de661d931f3da8bba
Dennis Durairaj <dennis.durairaj@gmail.com>
2020-03-12 21:38:38 +0100

    List items without key

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const items = [
+    { id: 1, value: "London" },
+    { id: 2, value: "Paris" },
+    { id: 3, value: "Tokyo" },
+    { id: 4, value: "Berlin" }
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {items.map(item => (
+        <div>{item.value}</div>
+      ))}
     </div>
   );
 }
