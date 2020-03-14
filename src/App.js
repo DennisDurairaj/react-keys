5e16056614bc779037bb98afac1b52b1269babee
Dennis Durairaj <dennis.durairaj@gmail.com>
2020-03-14 07:12:58 +0100

    create button to add items to the list

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  const items = [
+  const [items, setItems] = useState([]);
+  const itemList = [
     { id: 1, value: "London" },
     { id: 2, value: "Paris" },
     { id: 3, value: "Tokyo" },
     { id: 4, value: "Berlin" }
   ];
+
+  const addItem = () => {
+    const itemToAdd = itemList.find(item => {
+      const checkExists = items.filter(stateItem => {
+        return stateItem.id === item.id;
+      });
+      if (checkExists.length === 0) {
+        return item;
+      }
+    });
+    setItems([...items, itemToAdd]);
+  };
   return (
     <div className="App">
-      {items.map(item => (
-        <div key={item.id}>{item.value}</div>
-      ))}
+      <button disabled={items.length === itemList.length} onClick={addItem}>
+        Add item
+      </button>
+      {items.length > 0 &&
+        items.map(item => <div key={item.id}>{item.value}</div>)}
     </div>
   );
 }
